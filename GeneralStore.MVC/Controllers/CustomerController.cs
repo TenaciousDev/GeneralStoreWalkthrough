f229694737579ae5322412ad8ece3f23fe2c5344
A. S. Metcalf <70526588+asmetcalf@users.noreply.github.com>
2020-11-10 14:47:25 -0500

    Add project files.

---
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<Customer> customerList = _db.Customers.ToList();
+            List<Customer> orderedCustomerList = customerList.OrderBy(cust => cust.LastName).ToList();
+            return View(orderedCustomerList);
+        }
+        //GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+        //GET: Customer/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id is null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+            if (customer is null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        //POST: Customer/Delete/{id}
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _db.Customers.Find(id);
+            _db.Customers.Remove(customer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //GET: Customer/Edit/{id}
+        //POST: Customer/Edit/{id}
+
+        //GET: Customer/Detail/{id}
+    }
+}
