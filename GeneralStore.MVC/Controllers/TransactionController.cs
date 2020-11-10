623e3563566051b7ff803b425214aacef0f5b269
A. S. Metcalf <70526588+asmetcalf@users.noreply.github.com>
2020-11-10 17:13:24 -0500

    build nearly complete // NOTE: need to figure out how to hold a TimeStamp

--- GeneralStore.MVC/Controllers/TransactionController.cs
+++ GeneralStore.MVC/Controllers/TransactionController.cs
@@ -1,7 +1,9 @@
 ﻿using GeneralStore.MVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
@@ -35,5 +37,61 @@ namespace GeneralStore.MVC.Controllers
             }
             return View(transaction);
         }
+        //GET: Transaction/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            ViewBag.ProductId = new SelectList(_db.Products.ToList(), "ProductId", "Name");
+            ViewBag.CustomerId = new SelectList(_db.Customers.ToList(), "CustomerId", "FullName");
+
+            if (id is null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction is null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+        //POST: Transaction/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(transaction).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transaction);
+        }
+        //GET: Transaction/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id is null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction is null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+        //POST: Transaction/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            _db.Transactions.Remove(transaction);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Transaction/Details/{id}
     }
 }
